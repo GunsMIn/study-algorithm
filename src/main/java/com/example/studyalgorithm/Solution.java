7c8a9f39c28ada075ca9c8588f5a1ab93524b2e5
KimGunWoo9595 <medneter@naver.com>
2024-04-07 21:32:39 +0900

    초기 커밋

---
+++ src/main/java/com/example/studyalgorithm/Solution.java
@@ -0,0 +1,19 @@
+package src.main.java.com.example.studyalgorithm;
+
+public class Solution {
+
+
+    /**
+     *
+     * 1. X를 만나면 RESULT 에 담지 않고 ,
+     *`
+     *
+     * */
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
---
+++ src/test/java/com/example/studyalgorithm/StudyAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
