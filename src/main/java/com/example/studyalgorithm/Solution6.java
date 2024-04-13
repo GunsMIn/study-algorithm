eda96038c4537fb21b84015394e111c06700c971
KimGunWoo9595 <medneter@naver.com>
2024-04-13 11:14:22 +0900

    하샤드 수

--- src/main/java/com/example/studyalgorithm/Solution6.java
+++ src/main/java/com/example/studyalgorithm/Solution6.java
@@ -2,11 +2,23 @@ package com.example.studyalgorithm;
 
 public class Solution6 {
 
+    //하샤드 수
+    public boolean solution(int x) {
+        int sum = 0;
+        int original = x;  // 원래 숫자를 저장해두고
 
-   /**
+        while (x > 0) {
+            sum += x % 10;  // 각 자릿수를 더하기
+            x /= 10;
+        }
+
+        return original % sum == 0;  // 원래 숫자를 자릿수의 합으로 나누어 떨어지는지 검사
+    }
+
+    /**
     * 우선 각자리를 추출해야함 그렇기 위해서는 x 를 string으로 변환
     * */
-   public boolean solution(int x) {
+   public boolean solution2(int x) {
 
        int sum = 0;
        String s = String.valueOf(x);
